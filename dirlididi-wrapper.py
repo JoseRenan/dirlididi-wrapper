0234bd744a292fa2c5c00462ac51956dcadb6c04
José Renan <joserenansl99@gmail.com>
2017-10-21 01:26:09 -0300

    Added update option

--- dirlididi-wrapper.py
+++ dirlididi-wrapper.py
@@ -11,29 +11,40 @@ USER_HOME = expanduser('~')
 DIRLIDIDI_HOME = USER_HOME + '/.dirlididi-wrapper'
 
 
+def dirlididi_path(filename):
+	if not os.path.exists(DIRLIDIDI_HOME):
+		os.makedirs(DIRLIDIDI_HOME)
+	return os.path.join(DIRLIDIDI_HOME, filename)
+
+
+def update():
+	print('Atualizando dirlididi...')
+	dirlididi_path = dirlididi_path('dirlididi-wrapper.py')
+	urllib.urlretrieve('https://raw.githubusercontent.com/JoseRenan/dirlididi-wrapper/master/dirlididi-wrapper.py', dirlididi_path)
+	print('Atualizanção concluída.')
+
+
 def setup(user_token):
 
 	print('Baixando dirlididi.py...')	
 
-	if not os.path.exists(DIRLIDIDI_HOME):
-    		os.makedirs(DIRLIDIDI_HOME)
-	
+	path = dirlididi_path('dirlididi.py')
 	shutil.copy('./dirlididi-wrapper.py', DIRLIDIDI_HOME + '/dirlididi-wrapper.py')
-	dirlididi_path = os.path.join(DIRLIDIDI_HOME, 'dirlididi.py')
-	urllib.urlretrieve('http://dirlididi.com/tools/dirlididi.py', dirlididi_path)
+	urllib.urlretrieve('http://dirlididi.com/tools/dirlididi.py', path)
 	
 	print('Download concluído. Configurando ambiente...')
 
-	with open(USER_HOME + '/.bashrc', 'a') as bashrc:
-    		bashrc.write('\nexport DIRLIDIDI_HOME=' + DIRLIDIDI_HOME)
-		bashrc.write('\nexport DIRLIDIDI_USER_TOKEN=' + user_token)
-		bashrc.write('\nalias dirlididi="python $DIRLIDIDI_HOME/dirlididi-wrapper.py"')
+	if os.environ['DIRLIDIDI_HOME'] != None:
+		with open(USER_HOME + '/.bashrc', 'a') as bashrc:
+			bashrc.write('\nexport DIRLIDIDI_HOME=' + DIRLIDIDI_HOME)
+			bashrc.write('\nexport DIRLIDIDI_USER_TOKEN=' + user_token)
+			bashrc.write('\nalias dirlididi="python $DIRLIDIDI_HOME/dirlididi-wrapper.py"')
 
 	print('Perfeito. Pronto pra usar =D')
 
 def submit(problem_token, executable_name, source_name):
 	user_token = os.environ['DIRLIDIDI_USER_TOKEN']
-	dirlididi_path = os.environ['DIRLIDIDI_HOME'] + '/dirlididi.py'
+	path = os.environ['DIRLIDIDI_HOME'] + '/dirlididi.py'
 	os.system('bash -c "python %s submit %s %s %s %s"' %
 		(dirlididi_path, problem_token, user_token, executable_name, source_name))
 
@@ -41,7 +52,8 @@ def submit(problem_token, executable_name, source_name):
 def help():
 	print('Uso: dirlididi [OPÇÃO]')
 	print('Opções')
-	print('-i <token> - Instala e configura o dirlididi para submissões feitas com um determinado usuário que possui o token dado.')
+	print('-i <token> - (Re)instala e configura o dirlididi para submissões feitas com um determinado usuário que possui o token dado.')
+	print('-u - Atualiza o dirlididi-wrapper para a ultima versão.')
 	print('-s <prob_token> <exec_file> <source_file> - Submete um código e seu executável para um determinado problema.')
 	print('-h - Exibe as opções de uso.')
 
@@ -52,6 +64,8 @@ if __name__ == '__main__':
 		if len(sys.argv) == 3 and option == '-i':
 			user_token = sys.argv[2]
 			setup(user_token)
+		elif len(sys.argv) == 1 and option == '-u':
+			update()
 		elif len(sys.argv) == 4 and option == '-s':
 			problem_token = sys.argv[2]
 			executable_name = sys.argv[3]
