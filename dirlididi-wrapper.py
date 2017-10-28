e57d1da028cd44b31d457b1d228c0a145e297e7b
danielbgmitre <daniel.mitre@ccc.ufcg.edu.br>
2017-10-28 19:09:24 -0300

    adicionado opções de compilação #1

--- dirlididi-wrapper.py
+++ dirlididi-wrapper.py
@@ -10,16 +10,6 @@ import shutil
 USER_HOME = expanduser('~')
 DIRLIDIDI_HOME = USER_HOME + '/.dirlididi-wrapper'
 
-def identify_and_compile(filename):
-	sep = filename.rfind('.')
-	type_file = filename[sep+1:]
-	if type_file == 'cpp':
-		compile_cpp(filename[:sep])
-	elif type_file == 'hs':
-		compile_hs(filename[:sep])
-	elif type_file == 'pl':
-		compile_hs(filename[:sep])
-
 def dirlididi_path(filename):
     if not os.path.exists(DIRLIDIDI_HOME):
         os.makedirs(DIRLIDIDI_HOME)
@@ -39,8 +29,7 @@ def update():
     print('Atualizanção concluída.')
 
 
-def setup(user_token):
-
+def setup(user_token=None):
     print('Baixando dirlididi.py...')
 
     path = dirlididi_path('dirlididi.py')
@@ -49,13 +38,15 @@ def setup(user_token):
 
     print('Download concluído. Configurando ambiente...')
 
-    if os.environ.get('DIRLIDIDI_HOME') != None:
+    #if os.environ.get('DIRLIDIDI_HOME') != None:   #impedia de atualizar o user_token
+    if (user_token == None):
+        print('Perfeito. Pronto pra usar =D')
+    else:
         with open(USER_HOME + '/.bashrc', 'a') as bashrc:
             bashrc.write('\nexport DIRLIDIDI_HOME=' + DIRLIDIDI_HOME)
             bashrc.write('\nexport DIRLIDIDI_USER_TOKEN=' + user_token)
             bashrc.write('\nalias dirlididi="python $DIRLIDIDI_HOME/dirlididi-wrapper.py"')
-
-    print('Perfeito. Pronto pra usar =D')
+        print('Perfeito. Por favor, refaça o login no sistema antes de usar =D')
 
 def submit(problem_token, executable_name, source_name):
     user_token = os.environ['DIRLIDIDI_USER_TOKEN']
@@ -64,13 +55,40 @@ def submit(problem_token, executable_name, source_name):
         (path, problem_token, user_token, executable_name, source_name))
 
 
+def compile_cpp(filename):
+    os.system('bash -c "gcc %s.cpp -o %s"' %(filename, filename))
+
+def compile_hs(filename):
+    os.system('bash -c "ghc %s.hs -o %s"' %(filename, filename))
+    os.system('bash -c "rm %s.o"' %(filename))
+    os.system('bash -c "rm %s.hi"' %(filename))
+
+def identify_and_compile(filename):
+    sep = filename.rfind('.')
+    type_file = filename[sep+1:]
+    executable = filename[:sep]
+    if type_file == 'cpp':
+        compile_cpp(executable)
+        return executable
+    elif type_file == 'hs':
+        compile_hs(executable)
+        return executable
+    else:
+        return ''
+
+def autodetect_and_submit(problem_token, filename):
+    executable_name = identify_and_compile(filename)
+    submit(problem_token, executable_name, filename)
+
 def help():
     print('Uso: dirlididi [OPÇÃO]')
     print('Opções')
     print('-i <token> - (Re)instala e configura o dirlididi para submissões feitas com um determinado usuário que possui o token dado.')
+    print('-i - Recarrega os arquivos sem alterar o token de usuario')
     print('-u - Atualiza o dirlididi-wrapper para a ultima versão.')
     print('-s <prob_token> <exec_file> <source_file> - Submete um código e seu executável para um determinado problema.')
-	print('-c <source_file> - Identifica a linguagem pelo formato do arquivo e compila automaticamente.')
+    print('-c <source_file> - Identifica a linguagem pelo formato do arquivo e compila automaticamente.')
+    print('-cs <prob_token> <source_file> - Compila automaticamente e submete o arquivo')
     print('-h - Exibe as opções de uso.')
 
 
@@ -80,16 +98,32 @@ if __name__ == '__main__':
         if len(sys.argv) == 3 and option == '-i':
             user_token = sys.argv[2]
             setup(user_token)
+
+        elif len(sys.argv) == 2 and option == '-i':
+            setup()
+
         elif len(sys.argv) == 2 and option == '-u':
             update()
+
         elif len(sys.argv) == 5 and option == '-s':
             problem_token = sys.argv[2]
             executable_name = sys.argv[3]
             source_name = sys.argv[4]
             submit(problem_token, executable_name, source_name)
-		elif len(sys.argv) == 3 and option == '-c':
-			souce_name = sys.argv[2]
-			identify_and_compile(source_name)
+
+        elif len(sys.argv) == 4 and option == '-s':
+            problem_token = sys.argv[2]
+            executable_name = sys.argv[3]
+            submit(problem_token, executable_name, '')
+
+        elif len(sys.argv) == 3 and option == '-c':
+            source_name = sys.argv[2]
+            identify_and_compile(source_name)
+
+        elif len(sys.argv) == 4 and option == '-cs':
+            problem_token = sys.argv[2]
+            source_name = sys.argv[3]
+            autodetect_and_submit(problem_token, source_name)
 
         else:
             help()
